164a5abae24edcbb5109b874b3bbf784e20b55ec
Jonathan <j.sweet90@hotmail.com>
2019-04-08 12:48:49 -0500

    Added start_position rect along with getters/setters. Added needed Init() to initialize its sprite

--- Bop-the-Block-SDL/includes/Entity.h
+++ Bop-the-Block-SDL/includes/Entity.h
@@ -5,14 +5,21 @@
 class Entity
 {
 public:
+	bool Init(SDL_Renderer *renderer, std::string file_location);
 	bool IsOverlapping(SDL_Rect *collision_rect);
 	void SetPosition(int x, int y);
 	SDL_Rect GetPosition();
+	void SetStartPosition(int x, int y);
+	SDL_Rect GetStartPosition();
+	Sprite *GetSprite();
 
+	
 	virtual void Update() = 0;
 	virtual void Draw(SDL_Renderer *renderer) = 0;
+	virtual void Reset() = 0;
 
 protected:
+	SDL_Rect start_position;
 	SDL_Rect position;
 	Sprite sprite;
 };
--- Bop-the-Block-SDL/src/Entity.cpp
+++ Bop-the-Block-SDL/src/Entity.cpp
@@ -1,4 +1,16 @@
 #include "Entity.h"
+#include <iostream>
+
+bool Entity::Init(SDL_Renderer *renderer, std::string file_location)
+{
+	bool status = true;
+	if (!sprite.Init(renderer, file_location))
+	{
+		status = false;
+	}
+
+	return status;
+}
 
 bool Entity::IsOverlapping(SDL_Rect *collision_rect)
 {
@@ -16,10 +28,25 @@ bool Entity::IsOverlapping(SDL_Rect *collision_rect)
 
 void Entity::SetPosition(int x, int y)
 {
-	position = { x, y, position.w, position.h };
+	position = { x, y, sprite.GetDrawingArea().w, sprite.GetDrawingArea().h };
 }
 
 SDL_Rect Entity::GetPosition()
 {
 	return position;
+}
+
+void Entity::SetStartPosition(int x, int y)
+{
+	start_position = { x, y, sprite.GetDrawingArea().w, sprite.GetDrawingArea().h };
+}
+
+SDL_Rect Entity::GetStartPosition()
+{
+	return start_position;
+}
+
+Sprite *Entity::GetSprite()
+{
+	return &sprite;
 }
