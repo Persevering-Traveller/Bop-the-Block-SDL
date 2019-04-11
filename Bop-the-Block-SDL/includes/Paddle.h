1790aa9ad2b09c0ee35a16394ecb618e4b66326a
Jonathan <j.sweet90@hotmail.com>
2019-04-11 11:29:35 -0500

    Removed default constructor because there will only ever be one player object that needs info for its constructor; added x1 and x2 movement constraints; changed speed to MOVE_SPEED; added constants for sprite width and height

--- Bop-the-Block-SDL/includes/Paddle.h
+++ Bop-the-Block-SDL/includes/Paddle.h
@@ -7,19 +7,26 @@ enum class SPEED { LOW, MID, HIGH };
 class Paddle : public Entity
 {
 public:
-	Paddle();
+	Paddle(int start_x, int start_y, int x1_constraint, int x2_constraint);
 	void Move(int x_direction);
 	void ChangeSpeed(SPEED which_speed);
+
 	void Update(float delta_time);
 	void Draw(SDL_Renderer *renderer);
 	void Reset();
+
 private:
-	const int speed = 3;
-	SPEED current_max_speed;
+	const int SPRITE_WIDTH = 32;
+	const int SPRITE_HEIGHT = 16;
+
+	const int MOVE_SPEED = 3;
 	const int LOW_SPEED = 100;
 	const int MID_SPEED = 200;
 	const int HI_SPEED = 400;
 
+	int x1_constraint, x2_constraint;
+
+	SPEED current_max_speed;
 	int x_direction;
 	float x_position;
 	float velocity;
