97ae32f20275c17758f6c8e0f65747b55cdf671d
Jonathan <j.sweet90@hotmail.com>
2019-04-18 16:02:21 -0500

    Changed TEXT_WIDTH/_HEIGHT and changed the width of balls_text

--- Bop-the-Block-SDL/includes/GUI.h
+++ Bop-the-Block-SDL/includes/GUI.h
@@ -17,10 +17,10 @@ private:
 	const int ROW_START = 25;
 	const int ROW_OFFSET = 50;
 	const int TEXT_POINT_SIZE = 14;
-	const int TEXT_WIDTH = 50;
-	const int TEXT_HEIGHT = 10;
+	const int TEXT_WIDTH = 45;
+	const int TEXT_HEIGHT = 15;
 
-	const std::string TOP_SCORE_LABEL = "TOP SCORE\n";
+	const std::string TOP_SCORE_LABEL = "TOP\n";
 	const std::string SCORE_LABEL = "SCORE\n";
 	const std::string STAGE_LABEL = "STAGE\n";
 	const std::string BALLS_LABEL = "x ";
@@ -35,6 +35,6 @@ private:
 	Text top_score_text{ TOP_SCORE_LABEL + std::to_string(top_score_prev), COLUMN_CENTER, ROW_START, TEXT_WIDTH, TEXT_HEIGHT};
 	Text score_text{ SCORE_LABEL + std::to_string(score_prev), COLUMN_CENTER, ROW_START + ROW_OFFSET, TEXT_WIDTH, TEXT_HEIGHT };
 	Text stage_text{ STAGE_LABEL + std::to_string(stage_prev), COLUMN_CENTER, ROW_START + ROW_OFFSET * 2, TEXT_WIDTH, TEXT_HEIGHT };
-	Text balls_text{ BALLS_LABEL + std::to_string(balls_prev), COLUMN_CENTER, ROW_START + ROW_OFFSET * 3, TEXT_WIDTH, TEXT_HEIGHT };
+	Text balls_text{ BALLS_LABEL + std::to_string(balls_prev), COLUMN_CENTER, ROW_START + ROW_OFFSET * 3, TEXT_WIDTH/2, TEXT_HEIGHT };
 };
 #endif
