d29852ee2a428b2125710a1e5a5a6d1e99fe1a21
Jonathan <j.sweet90@hotmail.com>
2019-04-22 13:32:10 -0500

    Changed Init() to InitFont() and added InitStartLogo() for setting up the start screen logo; Now has a Sprite for the start screen logo

--- Bop-the-Block-SDL/includes/GUI.h
+++ Bop-the-Block-SDL/includes/GUI.h
@@ -2,14 +2,18 @@
 #define GUI_H
 #include <SDL_ttf.h>
 #include "Text.h"
+#include "Sprite.h"
 #include <string>
 
 class GUI
 {
 public:
-	bool Init(SDL_Renderer *renderer, std::string file_location);
+	bool InitFont(SDL_Renderer *renderer, std::string file_location);
+	bool InitStartLogo(SDL_Renderer *renderer, std::string file_location);
 	void Update(SDL_Renderer *renderer, int top, int score, int stage, int balls);
-	void Draw(SDL_Renderer *renderer);
+	void DrawStart(SDL_Renderer *renderer);
+	void DrawGameplay(SDL_Renderer *renderer);
+	void DrawGameOver(SDL_Renderer *renderer);
 	void Quit();
 
 private:
@@ -19,6 +23,10 @@ private:
 	const int TEXT_POINT_SIZE = 14;
 	const int TEXT_WIDTH = 45;
 	const int TEXT_HEIGHT = 15;
+
+	const int LOGO_WIDTH = 148;
+	const int LOGO_HEIGHT = 93;
+	const SDL_Rect START_LOGO_POSITION{ 90, 40, LOGO_WIDTH, LOGO_HEIGHT };
 
 	const std::string TOP_SCORE_LABEL = "TOP\n";
 	const std::string SCORE_LABEL = "SCORE\n";
@@ -30,6 +38,8 @@ private:
 	int score_prev = 0;
 	int stage_prev = 1;
 	int balls_prev = 3;
+
+	Sprite start_screen_logo{ 0, 0, LOGO_WIDTH, LOGO_HEIGHT };
 
 	TTF_Font *gui_font;
 	Text top_score_text{ TOP_SCORE_LABEL + std::to_string(top_score_prev), COLUMN_CENTER, ROW_START, TEXT_WIDTH, TEXT_HEIGHT};
